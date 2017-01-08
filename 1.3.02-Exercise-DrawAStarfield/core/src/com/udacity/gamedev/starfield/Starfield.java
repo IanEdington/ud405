27f0833d8c37dca0686a048a44438238139ab3fe
Ian Edington <IanEdington@gmail.com>
2017-01-07 23:04:58 -0500

    Finished Excercise 1.3.02
    Learning: instantiating a Generics object. Random numbers (int vs
              float). Importance of reading docs; I assumed that a specific
              Array<T> object worked the same as an arrays. For each loops.

--- 1.3.02-Exercise-DrawAStarfield/core/src/com/udacity/gamedev/starfield/Starfield.java
+++ 1.3.02-Exercise-DrawAStarfield/core/src/com/udacity/gamedev/starfield/Starfield.java
@@ -8,13 +8,9 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
 /**
- * TODO: Start here!
- *
  * In this exercise we'll draw a star field of white points on a black background. The number of
  * points will be defined by a density parameter that states what proportion of the pixels should be
  * white.
- *
- * TODO: Run what you've got before making any changes
  *
  * One thing to note is we're using two new LibGDX classes, Array, and Vector2. We're using a custom
  * Array type so LibGDX can control the memory, and avoid unfortunate garbage collection events.
@@ -39,18 +35,30 @@ public class Starfield extends ApplicationAdapter {
 
     @Override
     public void create() {
-        // TODO: Initialize a shapeRenderer
+        // Initialize a shapeRenderer
+        shapeRenderer = new ShapeRenderer();
 
-        // TODO: Call initStars
+        // Call initStars
+        initStars(STAR_DENSITY);
 
     }
 
     public void initStars(float density) {
-        // TODO: Figure out how many stars to draw. You'll need the screen dimensions, which you can get using Gdx.graphics.getWidth() and Gdx.graphics.getHeight().
+        // Figure out how many stars to draw. You'll need the screen dimensions, which you can get using Gdx.graphics.getWidth() and Gdx.graphics.getHeight().
+        int width = Gdx.graphics.getWidth();
+        int height = Gdx.graphics.getHeight();
 
-        // TODO: Create a new array of Vector2's to hold the star positions
+        // Create a new array of Vector2's to hold the star positions
+        int vectors = (int)(height * width * density);
+        stars = new Array<Vector2>(vectors);
 
-        // TODO: Use java.util.Random to fill the array of star positions
+        // Use java.util.Random to fill the array of star positions
+        java.util.Random random = new java.util.Random();
+        for (int i = 0; i < vectors; i++) {
+            float x = random.nextFloat() * width;
+            float y = random.nextFloat() * height;
+            stars.add(new Vector2(x, y));
+        }
 
     }
 
@@ -62,21 +70,28 @@ public class Starfield extends ApplicationAdapter {
 
     @Override
     public void render() {
-        // TODO: Make the night sky black
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        // Make the night sky black
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // TODO: Begin a shapeRenderer batch using ShapeType.Point
+        // Begin a shapeRenderer batch using ShapeType.Point
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Point);
+        shapeRenderer.setColor(255, 255, 255, 1);
 
-        // TODO: Loop through the star positions and use shapeRenderer to draw points
+        // Loop through the star positions and use shapeRenderer to draw points
+        for (Vector2 vector: stars) {
+            shapeRenderer.point(vector.x, vector.y, 0);
+        }
 
-        // TODO: End the shapeRenderer batch
+        // End the shapeRenderer batch
+        shapeRenderer.end();
 
     }
 
     @Override
     public void dispose() {
-        // TODO: Dispose of our ShapeRenderer
+        // Dispose of our ShapeRenderer
+        shapeRenderer.dispose();
 
         super.dispose();
     }
