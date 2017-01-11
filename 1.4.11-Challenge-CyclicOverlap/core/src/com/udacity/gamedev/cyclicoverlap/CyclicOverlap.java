4aecb8192de8e5e395f9925904f71ceff22960dc
Ian Edington <IanEdington@gmail.com>
2017-01-10 22:09:41 -0500

    Hack a 3d view with painters algorythem

--- 1.4.11-Challenge-CyclicOverlap/core/src/com/udacity/gamedev/cyclicoverlap/CyclicOverlap.java
+++ 1.4.11-Challenge-CyclicOverlap/core/src/com/udacity/gamedev/cyclicoverlap/CyclicOverlap.java
@@ -9,8 +9,6 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /**
- * TODO: Start here
- *
  * In this project we have three overlapping rectangles. They'd look nice and symmetrical if the
  * left end of the red one was on top of the blue one, but, constrained by the painter's algorithm
  * as we are, that would seem to be impossible. Can you figure out a way to make it happen?
@@ -56,7 +54,9 @@ public class CyclicOverlap extends ApplicationAdapter {
         renderer.setColor(Color.BLUE);
         renderer.rect(2, 3.5f, 3, 1.5f, 6, 1, 1, 1, 240);
 
-        // TODO: Make it look like the left end of RED is on top of BLUE
+        // Make it look like the left end of RED is on top of BLUE
+        renderer.setColor(Color.RED);
+        renderer.rect(2, 3.5f, 3, 1.5f, 3, 1, 1, 1, 0);
 
         renderer.end();
     }
