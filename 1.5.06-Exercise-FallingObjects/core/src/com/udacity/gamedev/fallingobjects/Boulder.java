4828f21a0e0fba48738d0e1f6acf044d9d704b01
Ian Edington <IanEdington@gmail.com>
2017-01-11 14:29:32 -0500

    Make bolders fall with gravity and windspeed

--- 1.5.06-Exercise-FallingObjects/core/src/com/udacity/gamedev/fallingobjects/Boulder.java
+++ 1.5.06-Exercise-FallingObjects/core/src/com/udacity/gamedev/fallingobjects/Boulder.java
@@ -13,7 +13,9 @@ public class Boulder {
     private static final float RADIUS_RATIO = 0.01f;
     private static final Color COLOR = Color.RED;
 
-    // TODO: Declare a constant holding the acceleration due to gravity. -20 works well
+    // Declare a constant holding the acceleration due to gravity. -20 works well
+    private static final float GRAVITY = 20;
+    private static final float WIND_SPEED = 3;
 
 
     Vector2 position;
@@ -28,8 +30,8 @@ public class Boulder {
     public void init(Viewport viewport){
         position = new Vector2();
 
-        // TODO: Set the initial velocity to zero in both directions
-        velocity = new Vector2(0, -200);
+        // Set the initial velocity to zero in both directions
+        velocity = new Vector2(0, 0);
 
         radius = viewport.getWorldWidth() * RADIUS_RATIO;
         position.y = viewport.getWorldHeight() + radius;
@@ -39,7 +41,9 @@ public class Boulder {
 
 
     public void update(float delta){
-        // TODO: Apply gravitational acceleration to the vertical velocity
+        // Apply gravitational acceleration to the vertical velocity
+        velocity.y -= GRAVITY;
+        velocity.x += WIND_SPEED;
 
 
         position.x += delta * velocity.x;
